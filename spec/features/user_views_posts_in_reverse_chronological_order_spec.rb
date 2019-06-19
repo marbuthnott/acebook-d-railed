6c6a1c8a6bce38c1613554b5591e954cba1e874e
Lucy Barber <lbarber729@hotmail.co.uk>
2019-06-19 12:21:10 +0100

    user story show posts in reverse chronological order

---
+++ spec/features/user_views_posts_in_reverse_chronological_order_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "Posts", type: :feature do
+  scenario "Views posts in reverse chronological  order" do
+    visit "/posts/new"
+    fill_in "Message", with: "Post number one"
+    click_button "Submit"
+
+    visit "/posts/new"
+    fill_in "Message", with: "This is the second post"
+    click_button "Submit"
+    expect(first(".post-text")).to have_content("This is the second post")
+  end
+end
