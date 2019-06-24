f673771b2611d72c3160581bc6d4d6d98bfe059d
VladC <vlad.chiriac24@gmail.com>
2019-06-24 12:47:31 +0100

    Added name and password confirmation
    Co-authored-by: lbarber729 <lbarber792@hotmail.co.uk>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+  validates :name, presence: true
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true,
+  validates :password, presence: true, confirmation: true,
                        length: { in: 6..10 }
 end
