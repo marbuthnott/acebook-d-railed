db0c4a18db3b4ced43481af7311b2275263dea1c
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-06-27 16:31:45 +0100

    Add home link
    Co-authored-by: Oliver Brownlow <olliebrownlow@gmail.com>

---
+++ spec/features/home_button_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Home button', type: :feature do
+  scenario 'Signup page has no home link' do
+    go_to_homepage
+
+    expect(page).not_to have_link('Home')
+  end
+
+  scenario 'Signin page has no home link' do
+    go_to_homepage
+    click_link('Sign in')
+
+    expect(page).not_to have_link('Home')
+  end
+
+  scenario 'User can navigate back to their wall from other walls' do
+    sign_up(
+      username: 'test',
+      email: 'test@test.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+    click_link('Members')
+    click_link('Home')
+
+    expect(page).to have_content('test\'s wall.')
+  end
+end
